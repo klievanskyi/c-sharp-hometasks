09fb1bf776cb2f2d1da8a379ae269e0b00844e9b
klievanskiy <nikolai.klevanski@gmail.com>
2022-06-24 17:05:53 +0300

    add condition of task1

--- sprint-1/Task1/Program.cs
+++ sprint-1/Task1/Program.cs
@@ -1,4 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//Please, create MyAccessModifiers class with next members:
+
+//int field birthYear is unavailable anywhere except this class
+
+//string field personalInfo that is accessible within descendants of this class
+
+//string field IdNumber that is accessible only within descendants in the current assembly
+
+//constructor should be available from everywhere in the program and accept int for birth year, string for idNumber, string for personalInfo parameters to initialize three fields mentioned above. (Note, that the order of parameters must be exact as described)
+
+//int property Age which returns the difference between the current year and birthYear and can be accessed everywhere, but only for reading
+
+//common for all instances of the class byte field averageMiddleAge with default value 50
+
+//string property Name accessible anywhere in the current assembly
+
+//string property NickName that can be read anywhere in the program and set only in the current assembly
+
+//method HasLivedHalfOfLife available only for descendants of the class in other assemblies and for all in the current.
+
+//overload operator ==. The operator returns true if name, age, and personalInfo  of objects are equal
+
 namespace Task1
 {
     class Program
