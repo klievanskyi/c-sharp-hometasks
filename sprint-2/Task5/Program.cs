6930d46435f02466ac44b2d7690d8c6c3214e530
klievanskiy <nikolai.klevanski@gmail.com>
2022-06-28 12:50:49 +0300

    add tasks 4 and 5 from sprint 2

---
+++ sprint-2/Task5/Program.cs
@@ -0,0 +1,33 @@
+﻿//For finish, let’s talk about the cats. The big cats.
+
+//Please, create class Acinonychini. This class has two sub-classes, that live now: Acinonyx and Puma.
+
+//Create these sub-classes too. It’s known that Puma has sub-classed and they may be created later. Acinonyx has no sub-classed, so it’s sub-classes can’t be created anywhen.
+
+//Create For all the classes those fields, properties and methods that you think they need.
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Acinonychini
+    {
+
+    }
+
+    class Puma : Acinonychini
+    {
+
+    }
+
+    sealed class Acinonyx : Acinonychini
+    {
+
+    }
+}
