f747e0f47728afc07188160cda70fdc6cb751d47
klievanskiy <nikolai.klevanski@gmail.com>
2022-06-30 23:37:30 +0300

    add task 1 from sprint-3

---
+++ sprint-3/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task1
+{
+
+    class CalcProgram
+    {
+        public delegate int CalcDelegate(int first, int second, char operation);
+
+        public CalcDelegate funcCalc = Calc;
+
+        public static int Calc(int first, int second, char operation)
+        {
+            int result = 0;
+            switch(operation)
+            {
+                case '-':
+                    result = first - second;
+                    break;
+                case '+':
+                    result = first + second;
+                    break;
+                case '*':
+                    result = first * second;
+                    break;
+                case '/':
+                    result = second <= 0 ? result : first / second;
+                    break;
+            }
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
