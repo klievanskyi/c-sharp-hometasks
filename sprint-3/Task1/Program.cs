27337665c5cf6f85fab4d5476c38641bfaa9597b
klievanskiy <nikolai.klevanski@gmail.com>
2022-07-01 23:27:14 +0300

    add condition of Task1 from sprint-3

--- sprint-3/Task1/Program.cs
+++ sprint-3/Task1/Program.cs
@@ -1,4 +1,14 @@
-﻿namespace Task1
+﻿//Create a program that can be used for calculation of 4 arithmetic operations (+, -, *, /) according to tasks:
+
+//1) declare a delegate CalcDelegate referring to a function Calc with three parameters (two numbers and one - operation sign) and a numerical result;
+
+//2) define a class CalcProgram and within this class:
+
+//2.1) define a static function Calc for computation with the same signature as the delegate. Note: in case of denominator = 0, the function returns 0.
+
+//2.2) create a public object funcCalc of delegate type and pass the function Calc as an argument.
+
+namespace Task1
 {
 
     class CalcProgram
