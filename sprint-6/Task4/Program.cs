a2f2c5dfb20a56507175fa1f3b917846d4226d77
klievanskiy <nikolai.klevanski@gmail.com>
2022-07-13 21:47:35 +0300

    add condition of Task4

---
+++ sprint-6/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Within the class, ShowPowerRange create a static method PowerRanger() that takes in integer degree, start, finish. 
+
+//The method returns all the numbers from the range [start, finish] (inclusive and finish > 0 and start > 0) that are equal to the degree-th power of any positive integer.
+
+//In the case start > finish, or start < 0, or finish < 0 the method returns 0.
+
+//The method involves yielding the intermediate result on each iteration.
+
+
+//For example, when calling the method PowerRanger():
+//for (int item in PowerRanger(3, 1, 200))
+//{
+//    Console.WriteLine("{0} ", item);
+//}
+
+//Output:
+//1
+//8
+//64
+//125
