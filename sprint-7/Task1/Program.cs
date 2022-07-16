ebf529b1c4ba6d266795c9a4a50a5f75da3c3034
klievanskiy <nikolai.klevanski@gmail.com>
2022-07-16 12:06:29 +0300

    add task1 from sprint-7

---
+++ sprint-7/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Please, implement the SumOfElementsOddPositions method that returns sum of elements with odd indexes in the array of doubles
+
+//(You don't need to verify on null parameter value. Assume that parameter will always be not null)
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] num = new double[] { 2, 3, 4, 5 };
+            double sum = EvaluateSumOfElementsOddPositions(num);
+            Console.WriteLine(sum);
+        }
+
+        public static double EvaluateSumOfElementsOddPositions(double[] inputData)
+        {
+            return inputData.Where((x, i) => i % 2 != 0).Sum();
+        }
+    }
+}
