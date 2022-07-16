c4e8959ca98e061ff3699b18adc9dbb512128e24
klievanskiy <nikolai.klevanski@gmail.com>
2022-07-16 12:20:24 +0300

    add task2 from sprint-7

---
+++ sprint-7/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Please, implement EvaluateAggregate method that takes 
+
+//     an array of doubles as the first parameter,
+
+//     a delegate that defines an aggregate behavior as the second,
+
+//     a delegate that defines a condition for the index as the third parameter.
+
+//The method should return a result of an aggregate operation for the elements with indexes that satisfy the condition set by the third parameter
+
+//(You don't need to verify on null parameter values. Assume that parameters will always be not null)
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static double EvaluateAggregate(double[] inputData, Func<double, double, double> aggregate, Func<double, int, bool> predicate)
+        {
+            return inputData.Where(predicate).Aggregate(aggregate);
+        }
+    }
+}
