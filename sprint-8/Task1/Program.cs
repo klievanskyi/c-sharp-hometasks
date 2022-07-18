c913069a5b77202240b8bc94751cf9e3f3e87a6d
klievanskiy <nikolai.klevanski@gmail.com>
2022-07-18 23:27:42 +0300

    add task1 from sprint-8

---
+++ sprint-8/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class ParallelUtils<T, TR>
+    {
+        public T operand1;
+        public T operand2;
+        public Func<T, T, TR> func;
+        public TR Result { get; set; }
+
+        public ParallelUtils(Func<T, T, TR> func, T operand1, T operand2)
+        {
+            this.func = func;
+            this.operand1 = operand1;
+            this.operand2 = operand2;
+        }
+
+        public void StartEvaluation()
+        {
+            Thread thread = new Thread(() => { Result = func(operand1, operand2); });
+            thread.Start();
+        }
+
+        public void Evaluate()
+        {
+            Result = func(operand1, operand2);
+        }
+    }
+}
