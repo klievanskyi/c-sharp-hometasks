c7c5537fe2182e65d3478ef7b37bc026da34f4fe
klievanskiy <nikolai.klevanski@gmail.com>
2022-07-21 21:23:29 +0300

    add task1 from sprint-9

---
+++ sprint-9/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿//Suppose we have a class named Calc which has a method Seq
+//generating n-th member of a certain number sequence (starting from 1).
+
+//Define a class named CalcAsync.In this class:
+
+//Write an asynchronous static method PrintSeqAsync taking integer parameter n,
+//that prints out the following:
+//"Seq[X] = Y", where X is the value of input parameter n, Y is n-th member of the sequence.
+//Use void as return type.
+
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class CalcAsync
+    {
+        public static async void PrintSeqAsync(int n)
+        {
+            await Task.Run(() => Console.WriteLine($"Seq[{n}] = {Calc.Seq(n)}"));
+        }
+    }
+}
